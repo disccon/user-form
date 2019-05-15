5baa084995779e01b4db08fa1934b5d000a2dc53
discconW <dissccon@gmail.com>
2019-05-15 11:13:42 +0300

    IndexDB_Initial

--- src/db.js
+++ src/db.js
@@ -1,7 +1,23 @@
 import Dexie from 'dexie'
+import { users } from './stubs/users'
+import { newUser } from './stubs/newUser'
 
 const db = new Dexie('dbNewUser')
 db.version(1).stores({ newUserDB: '++id' })
 db.version(1).stores({ listUserDB: '++id' })
+
+db.newUserDB.get(0, newUserDB => {
+  if (!newUserDB) {
+    db.newUserDB.add(newUser)
+  }
+})
+
+db.listUserDB.toArray(listUserDB => {
+  if (listUserDB.length <= 0) {
+    users.forEach(item => {
+      db.listUserDB.add(item)
+    })
+  }
+})
 
 export default db
